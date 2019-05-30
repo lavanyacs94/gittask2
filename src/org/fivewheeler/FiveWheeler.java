26046fce8a4cab4e195dd579d7dbb786141d8f1b
lavanyacs94 <prathapzenith@gmail.com>
2019-05-31 00:42:52 +0530

    added new class

---
+++ src/org/fivewheeler/FiveWheeler.java
@@ -0,0 +1,18 @@
+package org.fivewheeler;
+
+public class FiveWheeler {
+	
+	
+	
+	public void newcar() {
+System.out.println("This is new invention");
+	}
+	
+	public static void main(String[] args) {
+		
+		FiveWheeler FW= new FiveWheeler();
+		FW.newcar();
+		
+	}
+
+}
